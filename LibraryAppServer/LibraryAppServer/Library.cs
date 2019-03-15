123dd5745415ef16ab7c8480063b7813008662dc
unknown <ciara.walsh.14@gmail.com>
2019-03-15 19:49:42 +0000

    Book class created with title,description,rating and stock

---
+++ LibraryAppServer/LibraryAppServer/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAppServer
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public int Stock { get; set; }
+
+        public Book
+    }
+}
