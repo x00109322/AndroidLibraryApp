89d2b740970e40b0eb9cef5f83942912eed8a850
Dónal McManus <donal.mcm@gmail.com>
2019-03-15 19:20:34 +0000

    server-side created with swagger and a working test api

---
+++ LibraryAppServer/LibraryAppServer/Controllers/LibraryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAppServer.Controllers
+{
+    [Route("api/library")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        // test - hello world
+        [HttpGet("test")]
+        public string RunTestApi()
+        {
+            return "Hello World";                   // 200 OK
+        }
+    }
+}
