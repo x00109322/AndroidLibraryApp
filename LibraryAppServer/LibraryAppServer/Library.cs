4f5a8c086cd3c16d5d6c9408866b534378689953
unknown <ciara.walsh.14@gmail.com>
2019-03-25 20:48:52 +0000

    Add Key and Required to book class

--- LibraryAppServer/LibraryAppServer/Library.cs
+++ LibraryAppServer/LibraryAppServer/Library.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,18 +8,25 @@ namespace LibraryAppServer
 {
     public class Book
     {
+        [Key]
+        public string ISBN { get; set; }            //Unique key for international standard book number
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
+        public string Genre { get; set; }
         public string Description { get; set; }
         public double Rating { get; set; }
         public int Stock { get; set; }
 
-        public Book(string title,string author,string desctiption, double rating,int stock)
+        public Book(string title,string author,string desctiption, double rating,string genre, int stock)
         {
             this.Title = title;
             this.Author = author;
             this.Description = desctiption;
             this.Rating = rating;
+            this.Genre = genre;
             this.Stock = stock;
         }
     }
