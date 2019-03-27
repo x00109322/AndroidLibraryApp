ce4bc32c75b0ec426a6a9d118710a9f361627f5a
donalmcm <donal.mcm@gmail.com>
2019-03-27 20:21:02 +0000

    functions added to display books by genre

--- LibraryAppClient/app/src/main/java/com/libraryapp/libraryappclient/LibraryApi.java
+++ LibraryAppClient/app/src/main/java/com/libraryapp/libraryappclient/LibraryApi.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface LibraryApi {
 
-    @GET("api/books/all")
+    @GET("books/all")
     Call<List<Book>> getBooks();
+
+    @GET("books/genre/{genre}")
+    Call<List<Book>> getBooksByGenre(@Path("genre") String genre);
+
+    // @GET("books/title/{title}")
+    // @GET("books/booksOrderedByRating")
+    // @GET("books/booksOrderedByTitle")
+    // @GET("books/booksOrderedByAuthor")
+    // @GET("books/author/{author}")
+    // @GET("books/rating/{rating}")
 }
--- LibraryAppClient/app/src/main/java/com/libraryapp/libraryappclient/MainActivity.java
+++ LibraryAppClient/app/src/main/java/com/libraryapp/libraryappclient/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
 
+    private LibraryApi libraryApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +26,50 @@ public class MainActivity extends AppCompatActivity {
         textViewResult =findViewById(R.id.text_view_result);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://libraryappserver.azurewebsites.net/")
+                .baseUrl("https://libraryappserver.azurewebsites.net/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        LibraryApi libraryApi = retrofit.create(LibraryApi.class);
+        libraryApi = retrofit.create(LibraryApi.class);
 
+        getBooks();
+        //getBooksByGenre();
+    }
+
+    private void getBooks() {
         Call<List<Book>> call = libraryApi.getBooks();
+
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+
+                List<Book> books = response.body();
+
+                for(Book book : books) {
+                    String content = "";
+                    content += "ISBN: " + book.getIsbn() + "\n";
+                    content += "Title: " + book.getTitle() + "\n";
+                    content += "Author: " + book.getAuthor() + "\n";
+                    content += "Genre: " + book.getGenre() + "\n";
+                    content += "Rating: " + book.getRating() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getBooksByGenre() {
+        Call<List<Book>> call = libraryApi.getBooksByGenre("Novel");
 
         call.enqueue(new Callback<List<Book>>() {
             @Override
